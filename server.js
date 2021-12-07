7ffbf3f98b517f0f8582553792c31e57cf856d33
ayyb <ayyb@kakao.com>
2021-12-07 14:09:56 +0900

    mongodb setting & insert 기능

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
-const express = require('express');
-const bodyParser= require('body-parser') //express 기본 설치
+const express = require("express");
+const bodyParser = require("body-parser"); //express 기본 설치
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true})) //위치 잘 정리할것.
+app.use(bodyParser.urlencoded({ extended: true })); //위치 잘 정리할것.
+var pass = "qwer1234";
+var db
+MongoClient.connect(
+  `mongodb+srv://admin:${pass}@cluster0.fjbod.mongodb.net/todoapp?retryWrites=true&w=majority`,
+  { useUnifiedTopology: true },
+  function (error, client) {
+    if (error) return console.log(error);
+    db = client.db('todoapp'); //Mongo db 접속 방법
 
-app.listen(8080, function() {
-    console.log('listening on 8080')
-})
-app.get('/',function(req,res){
-    res.sendFile(__dirname+ '/index.html')
-})
-app.get('/write',function(req,res){
-    res.sendFile(__dirname+ '/write.html')
-})
-app.get('/pet',function(req,res){
-    res.send('펫용품 사세요')
-})
-app.get('/beauty',function(req,res){
-    res.send('뷰티용품 사세요')
-})
-app.post('/add',function(req,res){
-    console.log('add Page',req.body)
-    res.send('응답되었습니다.')
-})
+    //db insert 방법 db.collection('콜렉션명').insertOn({obj},callback(e,res){function})
+    db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(error, result){
+	    console.log('저장완료'); 
+	});
 
+    //서버띄우는 코드 여기로 옮기기
+    app.listen("8080", function () {
+      console.log("listening on 8080");
+    });
+  }
+);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+app.get("/pet", function (req, res) {
+  res.send("펫용품 사세요");
+});
+app.get("/beauty", function (req, res) {
+  res.send("뷰티용품 사세요");
+});
+app.post("/add", function (req, res) {
+  console.log("add Page", req.body);
+  res.send("응답되었습니다.");
+});
