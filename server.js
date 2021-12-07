f14fdf6d168303a0ac3012e69fcf280499e4391e
ayyb <ayyb@kakao.com>
2021-12-07 10:58:28 +0900

    index 화면 작성

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const app = express();
 app.listen(8080, function() {
     console.log('listening on 8080')
 })
+app.get('/',function(req,res){
+    res.sendFile(__dirname+ '/index.html')
+})
+app.get('/pet',function(req,res){
+    res.send('펫용품 사세요')
+})
+app.get('/beauty',function(req,res){
+    res.send('뷰티용품 사세요')
+})
