c94c93ad56a254b92ec9979dee6857bf58f8d9c4
ayyb <ayyb@kakao.com>
2021-12-07 11:35:44 +0900

    post 데이터 넘기기

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser= require('body-parser') //express 기본 설치
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true})) //위치 잘 정리할것.
 
 app.listen(8080, function() {
     console.log('listening on 8080')
@@ -7,9 +10,17 @@ app.listen(8080, function() {
 app.get('/',function(req,res){
     res.sendFile(__dirname+ '/index.html')
 })
+app.get('/write',function(req,res){
+    res.sendFile(__dirname+ '/write.html')
+})
 app.get('/pet',function(req,res){
     res.send('펫용품 사세요')
 })
 app.get('/beauty',function(req,res){
     res.send('뷰티용품 사세요')
 })
+app.post('/add',function(req,res){
+    console.log('add Page',req.body)
+    res.send('응답되었습니다.')
+})
+
